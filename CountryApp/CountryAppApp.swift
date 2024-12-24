034c75d7ea526b75f54cb06e5c8a4b02fa49dcb4
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2024-12-24 13:15:43 -0300

    Initial Commit

---
+++ CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Mizael Douglas on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountryAppTests/CountryAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppTests.swift
+//  CountryAppTests
+//
+//  Created by Mizael Douglas on 24/12/24.
+//
+
+import Testing
+@testable import CountryApp
+
+struct CountryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
